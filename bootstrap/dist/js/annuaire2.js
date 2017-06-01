91ff4be923a8dc549d8ade5aa56a4b71ee7ec576
BTSSIO2SLAM <174721@supinfo.com>
2017-06-01 13:49:35 +0400

    Add files via upload

---
+++ bootstrap/dist/js/annuaire2.js
@@ -0,0 +1,140 @@
+$(document).on('click', '.panel-heading span.clickable', function(e){
+    var $this = $(this);
+  if(!$this.hasClass('panel-collapsed')) {
+    $this.parents('.panel').find('.panel-body').slideUp();
+    $this.addClass('panel-collapsed');
+    $this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+  } else {
+    $this.parents('.panel').find('.panel-body').slideDown();
+    $this.removeClass('panel-collapsed');
+    $this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+  }
+})
+
+
+$(document).ready( function () {
+    // On cache les sous-menus :
+    $(".navigation ul.subMenu").hide();
+    // On sélectionne tous les items de liste portant la classe "toggleSubMenu"
+
+    // et on remplace l'élément span qu'ils contiennent par un lien :
+    $(".navigation li.toggleSubMenu span").each( function () {
+        // On stocke le contenu du span :
+        var TexteSpan = $(this).text();
+        $(this).replaceWith('<a href="" title="Afficher le sous-menu">' + TexteSpan + '<\/a>') ;
+    } ) ;
+
+    // On modifie l'évènement "click" sur les liens dans les items de liste
+    // qui portent la classe "toggleSubMenu" :
+    $(".navigation li.toggleSubMenu > a").click( function () {
+        // Si le sous-menu était déjà ouvert, on le referme :
+        if ($(this).next("ul.subMenu:visible").length != 0) {
+            $(this).next("ul.subMenu").slideUp("normal");
+        }
+        // Si le sous-menu est caché, on ferme les autres et on l'affiche :
+        else {
+            $(".navigation ul.subMenu").slideUp("normal");
+            $(this).next("ul.subMenu").slideDown("normal");
+        }
+        // On empêche le navigateur de suivre le lien :
+        return false;
+    });    
+
+
+} ) ;
+
+$(document).ready(function () {
+        var url = window.location;
+    // Will only work if string in href matches with location
+        $('ul.nav a[href="' + url + '"]').parent().addClass('active');
+
+    // Will also work for relative and absolute hrefs
+        $('ul.nav a').filter(function () {
+            return this.href == url;
+        }).parent().addClass('active').parent().parent().addClass('active');
+    });
+
+    $(document).ready( function () {
+  // On cache les sous-menus :
+  $(".navigation ul.subMenu").hide();    
+} ) ;
+
+
+
+function surligne(champ, erreur)
+{
+   if(erreur)
+      champ.style.backgroundColor = "#fba";
+   else
+      champ.style.backgroundColor = "";
+}
+
+function verifTelephone(champ)
+{
+   if(champ.value.length != 10 && champ.value.length != 0)
+   {
+      surligne(champ, true);
+      return false;
+   }
+   else
+   {
+      surligne(champ, false);
+      return true;
+   }
+}
+
+function verifMobile(champ)
+{
+   if(champ.value.length != 10 && champ.value.length != 0)
+   {
+      surligne(champ, true);
+      return false;
+   }
+   var regex = /0692+[0-9]/;
+   if(!regex.test(champ.value))
+   {
+      surligne(champ, true);
+      return false;
+   }
+   else
+   {
+      surligne(champ, false);
+      return true;
+   }
+}
+
+function verifInterne(champ)
+{
+   if(champ.value.length != 4 && champ.value.length != 0)
+  {
+      surligne(champ, true);
+      return false;
+  }
+  var regex=/[0-9]/;
+  if(!regex.test(champ.value))
+   {
+      surligne(champ, true);
+      return false;
+   }
+   else
+   {
+      surligne(champ, false);
+      return true;
+   }
+}
+
+function verifMail(champ)
+{
+   var regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
+   if(!regex.test(champ.value) && champ.value.length != 0)
+   {
+      surligne(champ, true);
+      return false;
+   }
+   else
+   {
+      surligne(champ, false);
+      return true;
+   }
+}
+
